397963c7fd963213703360523b125ae4fa1d115b
Slava <s220160@gmail.com>
2022-08-13 00:46:57 +0600

    add Zadacha06 projeckt

---
+++ Zadacha006/Program.cs
@@ -0,0 +1,16 @@
+﻿using static System.Console;
+Clear();
+//Задача 6
+//Напишите программу, которая на вход принимает число и выдаёт,
+//является ли число чётным (делится ли оно на два без остатка).
+WriteLine("Введите число: ");
+int number = Convert.ToInt32 (ReadLine());
+if(number%2 == 0)
+{
+    Write("Число чётное");
+}
+else
+{
+    WriteLine("Число нечётное");
+}
+WriteLine();
