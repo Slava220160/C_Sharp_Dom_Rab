daa6bb89f42852922beac06073b32f256a6ff0a5
Slava <s220160@gmail.com>
2022-08-13 01:04:21 +0600

    add Zadacha013 projeckt

---
+++ Zadacha013/Program.cs
@@ -0,0 +1,15 @@
+﻿using static System.Console;
+Clear();
+//Задача 13
+//Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+Write("Введите трёхзначное число: ");
+Double number = Convert.ToDouble(ReadLine ());
+if (number>99)
+{
+Double number1= number % 10;
+WriteLine($"Третий знак числа {number} - это {number1} ");
+}
+else
+{
+WriteLine("Введено число без третьего знака");
+}
