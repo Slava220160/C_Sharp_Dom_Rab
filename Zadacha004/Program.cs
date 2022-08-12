8f15b0fb387ea56c23e6ec8f8f5cab7d94e6eb54
Slava <s220160@gmail.com>
2022-08-13 00:41:18 +0600

    add Zadacha04 projeckt

---
+++ Zadacha004/Program.cs
@@ -0,0 +1,30 @@
+﻿using static System.Console;
+Clear();
+//Задача 4
+//Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+WriteLine("Введите число 1: ");
+int number1 = Convert.ToInt32 (ReadLine());
+WriteLine("Введите число 2: ");
+int number2 = Convert.ToInt32 (ReadLine());
+WriteLine("Введите число 3: ");
+int number3 = Convert.ToInt32 (ReadLine());
+
+if(number1>=number2)
+{
+   if(number1>=number3)
+   {
+        Write($"Максимальное число {number1}");
+   }
+}
+else
+{
+    if(number2>=number3)
+        {
+            Write($"Максимальное число {number2}");
+        }   
+    else 
+    {
+        Write($"Максимальное число {number3}");
+    }
+}
+WriteLine();
