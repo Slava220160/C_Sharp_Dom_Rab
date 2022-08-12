bfe377ea2215ff531c7c4f01ac1c91e828ef223b
Slava <s220160@gmail.com>
2022-08-13 00:51:29 +0600

    add Zadacha08 projeckt

---
+++ Zadacha008/Program.cs
@@ -0,0 +1,17 @@
+﻿using static System.Console;
+Clear();
+//Задача 8
+//Напишите программу, которая на вход принимает число (N),
+//а на выходе показывает все чётные числа от 1 до N.
+WriteLine("Введите число: ");
+int number = Convert.ToInt32 (ReadLine());
+int i=1;
+while(i<=number)
+{
+    if(i%2 == 0)
+    {
+        Write($"{i}, ");
+    }
+    i++;
+}
+WriteLine();
