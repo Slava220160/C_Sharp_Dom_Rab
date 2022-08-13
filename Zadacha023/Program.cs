929484a3b8605babaa5f23b5e264013ceed095a3
Slava <s220160@gmail.com>
2022-08-13 21:15:22 +0600

    add Zadacha023 projeckt

---
+++ Zadacha023/Program.cs
@@ -0,0 +1,14 @@
+﻿using static System.Console;
+Clear();
+//Задача 23
+// Напишите программу, которая принимает на вход число N и выдает таблицу кубов чисел от 1 до N
+Write("Введите число: ");
+int N = int.Parse(ReadLine()!);
+int i = 1;
+
+while (i <= N)
+    {
+    Write($"{i*i*i} ");
+    i++;
+    }
+WriteLine();
