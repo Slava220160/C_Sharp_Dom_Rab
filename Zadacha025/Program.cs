db3673ddbd83e7550031d48c8615e773ea03546b
Slava <s220160@gmail.com>
2022-08-22 20:43:56 +0600

    add Zadacha025 projeckt

---
+++ Zadacha025/Program.cs
@@ -0,0 +1,25 @@
+﻿using static System.Console;
+Clear();
+// Задача 25: Напишите цикл, который проводится на два входа (A и B)
+WriteLine("Введите числа через пробел ");
+int[] array = GetArrayFromString(ReadLine());
+WriteLine($"{array[0]} в степени {array[1]} равно {GetPower(array)}");
+int[] GetArrayFromString(string arrayString)
+{
+    string[] massString = arrayString.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+    int[] result = new int[massString.Length];
+    for (int i = 0; i < result.Length; i++)
+    {
+        result[i] = int.Parse(massString[i]);
+    }
+    return result;
+}
+int GetPower(int[] Massiv)
+{
+    int power = 1;
+    for (int i = 1; i <= Massiv[1]; i++)
+    {
+        power = power * Massiv[0];
+    }
+    return power;
+}
