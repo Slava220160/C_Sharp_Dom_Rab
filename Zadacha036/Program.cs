42439356a71fa4709c69429edc290d8851a5f693
Slava <s220160@gmail.com>
2022-08-22 22:13:02 +0600

    add Zadacha036 Projeckt

---
+++ Zadacha036/Program.cs
@@ -0,0 +1,28 @@
+﻿using static System.Console;
+Clear();
+//Задача 36
+// Задайте одномерный массив, заполненный случайными числами.
+// Найдите сумму элементов, стоящих на нечётных позициях.
+WriteLine("Задайте размер массива ");
+int ArrayCount = int.Parse(ReadLine());
+int[] Array= GetArray(ArrayCount);
+WriteLine($"{String.Join(" ", Array)}");
+WriteLine($"Сумма элементов, стоящих на нечетных позициях {GetSum(Array)}");
+int[] GetArray(int Count)
+{
+    int[] result = new int[Count];
+for (int i = 0; i < Count; i++)
+{
+    result[i]= new Random().Next(-100,100);
+}
+    return result;
+}
+int GetSum(int[] massive)
+{
+    int Sum=0;
+for (int i = 1; i < massive.Length; i+=2)
+{
+    Sum+=massive[i];
+}
+    return Sum;
+}
