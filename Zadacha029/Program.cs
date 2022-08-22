443d65cae66cb5218a0e63837c8c1745864423ac
Slava <s220160@gmail.com>
2022-08-22 21:10:19 +0600

    add Zadacha029 projeckt

---
+++ Zadacha029/Program.cs
@@ -0,0 +1,15 @@
+﻿using static System.Console;
+Clear();
+//Задача 29
+// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+int[] Mass = GetArray(8);
+WriteLine($"[{String.Join(", ", Mass)}]");
+int[] GetArray(int N)
+{
+int[] array = new int[N];
+for (int i=0; i<N; i++)
+{
+    array[i] = new Random().Next(0,10);
+}
+return array;
+}
