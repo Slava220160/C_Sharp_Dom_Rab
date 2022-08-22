8606d511b9f8a8d79aeb135d057944d83c81d121
Slava <s220160@gmail.com>
2022-08-22 22:14:04 +0600

    add Zadacha038 Projeckt

---
+++ Zadacha038/Program.cs
@@ -0,0 +1,41 @@
+﻿using static System.Console;
+Clear();
+//Задача 38
+// Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+WriteLine("Задайте размер массива вещественных чисел ");
+int countArray = int.Parse(ReadLine());
+int[] Array = GetArray(countArray);
+WriteLine($"{String.Join(" ", Array)}");
+WriteLine($"Разница между максимальным {GetMax(Array)} и минимальным {GetMin(Array)} элементами массива равна {GetDifference(GetMax(Array),GetMin(Array))}");
+int[] GetArray(int count) 
+{
+    int[] result = new int[count];
+    for (int i = 0; i < count; i++)
+    {
+        result[i] = new Random().Next(-99, 100);
+    }
+    return result;
+}
+int GetMin(int[] MinMassive)
+{
+    int minimum = MinMassive[0];
+    for (int i = 1; i < MinMassive.Length; i++)
+    {
+       minimum=minimum>MinMassive[i]?MinMassive[i]:minimum;
+    }
+    return minimum;
+}
+int GetMax(int[] MaxMassive)
+{
+    int maximum=MaxMassive[0];
+for (int i = 1; i < MaxMassive.Length; i++)
+{
+    maximum=maximum<MaxMassive[i]?MaxMassive[i]:maximum;
+}
+    return maximum;
+}
+int GetDifference(int maxArray, int minArray)
+{
+   int difference=maxArray-minArray;
+   return difference;
+}
