84329eac38501a5c3f04128367d89a6f0a6098d3
Slava <s220160@gmail.com>
2022-09-01 14:00:40 +0600

    add Zadacha060 projeckt

---
+++ Zadacha060/Program.cs
@@ -0,0 +1,26 @@
+﻿using static System.Console;
+using System.Linq;
+Clear();
+
+/*Задача 60
+Сформируйте трёхмерный массив из неповторяющихся двузначных чисел.
+Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.*/
+
+int m = 2;
+int n = 2;
+int p = 2;
+
+int[,,] array = new int[m, n, p];
+
+for (int i = 0; i < m; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+       for (int k = 0; k < p; k++)
+       {
+        array[i, j, k] = new Random().Next(10);
+        Write($"{array[i, j, k]}({i},{j},{k}) ");
+       }
+        WriteLine();
+    }
+}
