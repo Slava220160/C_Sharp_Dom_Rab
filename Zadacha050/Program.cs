1e077546e248b83532f12a32232ff2a6a19f7a8b
Slava <s220160@gmail.com>
2022-09-01 13:01:18 +0600

    add Zadacha050 projeckt

---
+++ Zadacha050/Program.cs
@@ -0,0 +1,60 @@
+﻿using static System.Console;
+using System.Linq;
+Clear();
+
+/*Задача 50
+Напишите программу, которая на вход принимает позиции элемента в двумерном массиве,
+и возвращает значение этого элемента или же указание, что такого элемента нет.*/
+
+WriteLine("Введите размеры массива через пробел: ");
+int[] parametrs = ReadLine()!
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(x => int.Parse(x))
+                .ToArray();
+WriteLine();
+int[,] array = GetArray(parametrs[0], parametrs[1]);
+PrintArray(array);
+WriteLine();
+WriteLine("Введите позиции элемента в двумерном массиве через пробел: ");
+int[] coordinates = ReadLine()!
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(x => int.Parse(x))
+                .ToArray();
+FindElementsMatrix(coordinates, array);
+
+int[,] GetArray(int rows, int columns)
+{
+    int[,] result = new int[rows, columns];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            result[i, j] = new Random().Next(1, 10);
+        }
+    }
+    return result;
+}
+
+void PrintArray(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Write($"{matrix[i, j]} ");
+        }
+        WriteLine();
+    }
+}
+
+void FindElementsMatrix(int[] coordinatesElementMatrix, int[,] matrixForSearch)
+{
+    if (coordinatesElementMatrix[0] < (matrixForSearch.GetLength(0) + 1) && coordinatesElementMatrix[1] < (matrixForSearch.GetLength(1)))
+    {
+        WriteLine($"Искомый элемент: {matrixForSearch[coordinatesElementMatrix[0], coordinatesElementMatrix[1]]}");
+    }
+    else
+    {
+        WriteLine("Элемента с такой позицией в массиве нет");
+    }
+}
